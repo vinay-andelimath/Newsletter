8e3e1abfad7c9e9ae19ca67744d30efb52f5c381
vinay <vinay.andelimath@gmail.com>
2023-02-05 20:04:30 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+const app=express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+const firstName=req.body.fname;
+const lastName=req.body.lname;
+const email=req.body.email;
+
+const data={
+members:[
+    {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+            FNAME:firstName,
+            LNAME:lastName
+        }
+    }
+]
+
+};
+const jsonData=JSON.stringify(data);
+
+const url="https://us21.api.mailchimp.com/3.0/lists/deb0369b0c";
+
+const options={
+    method:"POST",
+    auth:"Vinay5:3929d5319de68b66983f2e7e6b1c8376-us21"
+}
+
+const request=https.request(url,options,function(response){
+if(response.statusCode===200){
+    res.sendFile(__dirname + "/success.html");
+}else{
+    res.sendFile(__dirname + "/failure.html");
+}
+
+
+
+
+response.on("data",function(data){
+    console.log(JSON.parse(data));
+})
+})
+request.write(jsonData);
+request.end();
+
+
+
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/")
+})
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+
+    console.log("Server is running on port 3000");
+})
+
+
+
+
+//3929d5319de68b66983f2e7e6b1c8376-us21
+//deb0369b0c
